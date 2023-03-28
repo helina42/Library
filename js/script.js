b7983c2907f9ce9ed992c859625762f236ceff9e
Helina Alebachew <helinaabebe42@gmail.com>
2023-03-29 00:19:43 +0900

    Add class for a functional library in js and add html styled with css

--- js/script.js
+++ js/script.js
@@ -1,40 +1,61 @@
-let bookLib = [];
-const book = {};
-function getBook() {
-  book.title = document.querySelector('.title').value;
-  book.author = document.querySelector('.author').value;
-  return book;
+class Book {
+  id = 0;
+
+  title = '';
+
+  author = '';
+
+  constructor(id, title, author) {
+    this.id = id;
+    this.author = author;
+    this.title = title;
+  }
 }
 
-function addBook(books) {
-  const bookLi = document.querySelector('#book-list');
-  const book = document.createElement('LI');
-  book.setAttribute('style', 'id:list');
-  book.innerHTML = `<h3> ${books.title} </h3> <p>${books.author} </p>`;
-  book.style.listStyle = 'none';
-  const delBook = document.createElement('button');
-  delBook.innerHTML = 'Delete';
-  delBook.addEventListener('click', () => {
-    book.innerHTML = '';
-  });
-  book.appendChild(delBook);
-  bookLi.appendChild(book);
+class bookLib {
+  bookList = [];
+
+  constructor(bookList) {
+    this.bookList = bookList;
+  }
+
+  addBook(book) {
+    this.bookList.push(book);
+    localStorage.setItem('bookLib', JSON.stringify(this.bookList));
+    UI(book);
+  }
+
+  removeBook(book) {
+    const newBooks = [];
+    for (let i = 0; i < this.bookList.length; i++) {
+      if (this.bookList[i].id !== book.id) {
+        newBooks.push(this.bookList[i]);
+      }
+    }
+    this.bookList = newBooks;
+    localStorage.setItem('bookLib', JSON.stringify(this.bookList));
+  }
 }
+
+function getBook() {
+  const title = document.querySelector('.title');
+  const author = document.querySelector('.author');
+  const book = new Book(myBookLib.bookList.length + 1, title.value, author.value);
+  title.value = '';
+  author.value = '';
+  return book;
+}
+const myBookLib = new bookLib([]);
+
 const addBtn = document.querySelector('.addBook');
 addBtn.addEventListener('click', () => {
   const book = getBook();
-  bookLib.push(book);
-  localStorage.setItem('bookLib ', JSON.stringify(bookLib));
-  addBook(book);
+  myBookLib.addBook(book);
 });
 
 window.onload = () => {
-  bookLib = JSON.parse(localStorage.getItem('bookLib' || '[]'));
-  if (bookLib === null) {
-    bookLib = [];
-    return;
-  }
-  bookLib.forEach((book) => {
-    addBook(book);
+  myBookLib.bookList = JSON.parse(localStorage.getItem('bookLib')) || [];
+  myBookLib.bookList.forEach((book) => {
+    UI(book);
   });
 };
