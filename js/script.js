f49fbcc57a8e9c9466025f8f76bef0eb384ca2e3
Helina Alebachew <helinaabebe42@gmail.com>
2023-03-28 19:07:37 +0900

    fix local storage after review

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-let bookLib = [];
-const book = {};
+let bookLib = JSON.parse(localStorage.getItem('bookLib')) || [];
 function getBook() {
+  const book = {};
   book.title = document.querySelector('.title').value;
   book.author = document.querySelector('.author').value;
+  book.id = '';
+
+  if (bookLib === null) {
+    book.id = 0;
+  } else {
+    book.id = bookLib.length;
+  }
   return book;
 }
-
 function addBook(books) {
   const bookLi = document.querySelector('#book-list');
   const book = document.createElement('LI');
   book.setAttribute('style', 'id:list');
+  book.id = `${books.id}`;
   book.innerHTML = `<h3> ${books.title} </h3> <p>${books.author} </p>`;
   book.style.listStyle = 'none';
   const delBook = document.createElement('button');
   delBook.innerHTML = 'Delete';
   delBook.addEventListener('click', () => {
-    book.innerHTML = '';
+    const filtered = bookLib.filter((book) => book.id !== books.id);
+    localStorage.setItem('bookLib', JSON.stringify(filtered));
+    window.location.reload();
   });
   book.appendChild(delBook);
   bookLi.appendChild(book);
@@ -26,6 +35,7 @@ addBtn.addEventListener('click', () => {
   bookLib.push(book);
   localStorage.setItem('bookLib', JSON.stringify(bookLib));
   addBook(book);
+  window.location.reload();
 });
 
 window.onload = () => {
